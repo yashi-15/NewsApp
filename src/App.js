947f708061d69b37e31a2b9da4b2882c72628eea
yashi-15 <sharmayashi270@gmail.com>
2024-04-07 16:26:04 +0530

    hidden api key

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import News from "./components/News";
 import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
+  apikey = process.env.REACT_APP_NEWS_API_KEY
   state = {
     progress: 0,
   };
@@ -23,13 +24,13 @@ render() {
         progress={this.state.progress}
       />
           <Routes>
-            <Route exact path="/" element={<News setProgress = {this.setProgress} key = "general" apikey="4d18a59c3b5e4919a996e6c84467c007" country="in" category="general" pagesize="20" />} />
-            <Route exact path="/entertainment" element={<News setProgress = {this.setProgress} key = "entertainment" apikey="4d18a59c3b5e4919a996e6c84467c007" country="in" category="entertainment" pagesize="20" />} />
-            <Route exact path="/health" element={<News setProgress = {this.setProgress} key = "health" apikey="4d18a59c3b5e4919a996e6c84467c007" country="in" category="health" pagesize="20" />} />
-            <Route exact path="/business" element={<News setProgress = {this.setProgress} key = "business" apikey="4d18a59c3b5e4919a996e6c84467c007" country="in" category="business" pagesize="20" />} />
-            <Route exact path="/science" element={<News setProgress = {this.setProgress} key = "science" apikey="4d18a59c3b5e4919a996e6c84467c007" country="in" category="science" pagesize="20" />} />
-            <Route exact path="/sports" element={<News setProgress = {this.setProgress} key = "sports" apikey="4d18a59c3b5e4919a996e6c84467c007" country="in" category="sports" pagesize="20" />} />
-            <Route exact path="/technology" element={<News setProgress = {this.setProgress} key = "technology" apikey="4d18a59c3b5e4919a996e6c84467c007" country="in" category="technology" pagesize="20" />} />
+            <Route exact path="/" element={<News setProgress = {this.setProgress} key = "general" apikey={this.apikey} country="in" category="general" pagesize="20" />} />
+            <Route exact path="/entertainment" element={<News setProgress = {this.setProgress} key = "entertainment" apikey={this.apikey} country="in" category="entertainment" pagesize="20" />} />
+            <Route exact path="/health" element={<News setProgress = {this.setProgress} key = "health" apikey={this.apikey} country="in" category="health" pagesize="20" />} />
+            <Route exact path="/business" element={<News setProgress = {this.setProgress} key = "business" apikey={this.apikey} country="in" category="business" pagesize="20" />} />
+            <Route exact path="/science" element={<News setProgress = {this.setProgress} key = "science" apikey={this.apikey} country="in" category="science" pagesize="20" />} />
+            <Route exact path="/sports" element={<News setProgress = {this.setProgress} key = "sports" apikey={this.apikey} country="in" category="sports" pagesize="20" />} />
+            <Route exact path="/technology" element={<News setProgress = {this.setProgress} key = "technology" apikey={this.apikey} country="in" category="technology" pagesize="20" />} />
           </Routes>
         </BrowserRouter>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-// import PropTypes from "prop-types";
 import {Link} from 'react-router-dom'
 
 export class Navbar extends Component {
-  // static propTypes = {};
 
   render() {
     return (
